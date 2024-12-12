d00534a38a56f6bad7dfa868c7df76475097d2f2
Erwan <44072181+Charlemagne8378@users.noreply.github.com>
2024-12-12 15:56:22 +0100

    Update vikings.sql

--- resources/vikings.sql
+++ resources/vikings.sql
@@ -24,46 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `weapon`
+--
+
+CREATE TABLE `weapon` (
+  `id` int NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `damage` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour la table `weapon`
+--
+ALTER TABLE `weapon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour la table `weapon`
+--
+ALTER TABLE `weapon`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Insertion des armes dans la table `weapon` avec des noms anglais
+--
+INSERT INTO `weapon` (`type`, `damage`) VALUES
+('Axe', 50),
+('Sword', 40),
+('Spear', 35),
+('Bow', 25),
+('Hammer', 60),
+('Dagger', 20);
+
+--
 -- Structure de la table `viking`
 --
 
 CREATE TABLE `viking` (
   `id` int NOT NULL,
   `name` varchar(16) NOT NULL,
+  `health` int NOT NULL,
   `attack` int NOT NULL,
   `defense` int NOT NULL,
-  `health` int NOT NULL
+  `weaponID` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `viking`
 --
 
-INSERT INTO `viking` (`id`, `name`, `attack`, `defense`, `health`) VALUES
-(1, 'Ragnar', 200, 150, 300),
-(2, 'Floki', 150, 80, 350),
-(3, 'Lagertha', 300, 200, 200),
-(4, 'Björn', 350, 200, 100);
+INSERT INTO `viking` (`id`, `name`, `health`, `attack`, `defense`, `weaponID`) VALUES
+(1, 'Ragnar', 200, 150, 300, 2),   -- Sword
+(2, 'Floki', 150, 80, 350, 1),     -- Axe
+(3, 'Lagertha', 300, 200, 200, 3), -- Spear
+(4, 'Björn', 350, 200, 100, 5);     -- Hammer
 
---
--- Index pour les tables déchargées
+--		
+-- Index pour les tables déchargées		
 --
 
 --
 -- Index pour la table `viking`
 --
 ALTER TABLE `viking`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `weaponID` (`weaponID`);
 
 --
 -- AUTO_INCREMENT pour la table `viking`
 --
 ALTER TABLE `viking`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour la table `viking`
+--
+ALTER TABLE `viking`
+  ADD CONSTRAINT `viking_ibfk_1` FOREIGN KEY (`weaponID`) REFERENCES `weapon` (`id`);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
